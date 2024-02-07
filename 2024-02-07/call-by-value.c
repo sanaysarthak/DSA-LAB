d1ac2a8b1e25e5e841a2e28b08e0c99689b3072b
sanaysarthak <66907138+sanaysarthak@users.noreply.github.com>
2024-02-07 16:42:27 +0530

    Create call-by-value.c

---
+++ 2024-02-07/call-by-value.c
@@ -0,0 +1,27 @@
+// Practical 1.(a) Write a C program to implement call by value
+
+// Program to swap two numbers using call-by value in C
+#include <stdio.h>
+int swap(int p, int q) 
+{
+    printf("\nValues after swapping in SWAP function:-\n");
+    int temp= p;
+    p = q;
+    q = temp;
+    printf("a = %d\t b = %d", p, q);
+}
+
+int main()
+{
+    int a, b;
+    printf("Enter 1st number: ");
+    scanf("%d", &a);
+    printf("Enter 2nd number: ");
+    scanf("%d", &b);
+    printf("Original values before swapping:-\n");
+    printf("a = %d\t b = %d", a, b);
+    swap(a, b);
+    printf("\nValues after swapping in main function:-\n");
+    printf("a = %d\t b = %d", a, b);
+    return 0;
+}
