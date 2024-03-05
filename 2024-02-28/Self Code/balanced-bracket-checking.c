dfde27c4cb60b44f93d6ce74552177072beb371d
Sarthak Sanay <66907138+sanaysarthak@users.noreply.github.com>
2024-03-05 19:59:49 +0530

    Create balanced-bracket-checking.c
    initializing the file with idea

---
+++ 2024-02-28/Self Code/balanced-bracket-checking.c
@@ -0,0 +1,4 @@
+// Code to check balanced brackets in an expression
+// Balanced Input: [()]{}{[()()]()} -> Balanced
+// Unbalanced Input : [(]) // 1 and 4 brackets are not balanced because there is a closing ‘]’ before the closing ‘(‘
+
