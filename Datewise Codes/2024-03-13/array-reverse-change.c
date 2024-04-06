2bfcd6fc73c5c886737432f4fbc85bee0d80f0d4
Sarthak Sanay <66907138+sanaysarthak@users.noreply.github.com>
2024-04-06 19:55:31 +0530

    Update array-reverse-change.c

--- Datewise Codes/2024-03-13/array-reverse-change.c
+++ Datewise Codes/2024-03-13/array-reverse-change.c
@@ -1,1 +1,36 @@
-// change the array elements actually, instead of just printing it in the reverse order
+// Program in C to reverse the elements of an array
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+    int arr[n];
+    for(int i=0; i<n; i++)
+    {
+        printf("Enter element %d: ",i);
+        scanf("%d", &arr[i]);
+    }
+  
+    printf("Original Array:-\n");
+    for(int i=0; i<n; i++)
+    {
+        printf("%d  ", arr[i]);
+    }
+  
+    // Reversing the Array
+    for(int i=0, j=n-1; i<j; i++, j--)
+    {
+        int temp= arr[i];
+        arr[i]= arr[j];
+        arr[j]= temp;
+    }
+  
+    printf("\nReverse of the Array:-\n");
+    for(int i=0; i<n; i++)
+    {
+        printf("%d  ", arr[i]);
+    }
+  
+    return 0;
+}
